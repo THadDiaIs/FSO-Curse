caeed34c229df7e587d7e9141191cdc19f51937b
AdolfHd <sdia7adia@gmail.com>
2023-12-01 06:40:46 +0700

    1.3 paso3 refactorizare el codigo de los componentes

--- Part1/1a-Ejercicios-1.1-1.2/src/index.js
+++ Part1/1a-Ejercicios-1.1-1.2/src/index.js
@@ -10,38 +10,46 @@ const Header = (props) => {
 }
 
 const Content = (props) => {
+  let part = props.part;
   return (
     <>
-      <p>{props.part} {props.excercises}</p>
+      <p>{part.name} {part.exercises}</p>
     </>
   )
 }
+
 const Total = (props) => {
   return (
     <>
-      <p>Number of excercises {porps.total}</p>
+      <p>Number of excercises {props.total}</p>
     </>
   )
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name : 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name:'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
-    <>
+    <div>
       <Header course={course} />
-      <Content part={part1} excercises={excercises1} />
-      <Content part={part2} excercises={excercises2} />
-      <Content part={part3} excercises={excercises3} />
-      <Total total={exercises1 + exercises2 + exercises3}/>
-    </>
+      <Content part={part1} />
+      <Content part={part2} />
+      <Content part={part3} />
+      <Total total={part1.exercises + part2.exercises + part3.exercises}/>
+    </div>
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'));
